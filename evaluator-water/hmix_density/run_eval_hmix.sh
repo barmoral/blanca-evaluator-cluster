b9890d7533b6fd0fedc27fc7e3abc7a318ea2935
Barbara Morales <bamo6610@colorado.edu>
2024-07-03 08:23:14 -0600

    Added python code and inputs to calculate hmix and density

---
+++ evaluator-water/hmix_density/run_eval_hmix.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --time=20:00:00
+#SBATCH --partition=blanca-shirts
+#SBATCH --qos=blanca-shirts
+#SBATCH --account=blanca-shirts
+#SBATCH --gres=gpu
+#SBATCH --job-name=hmix_dens
+#SBATCH --output=slurm_codes/hmix_dens.log
+
+module purge
+module avail
+ml anaconda
+conda activate evaluator-blanca
+
+python hmix_dens_calc.py
+echo "It finished"
