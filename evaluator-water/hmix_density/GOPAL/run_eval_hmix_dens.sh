917eea25378e561d90a1d6f8310912dd686f27a6
Barbara Morales <bamo6610@colorado.edu>
2024-07-31 13:56:57 -0600

    Edited opc offxml to calculate hmix and dens for GOPAL water model

---
+++ evaluator-water/hmix_density/GOPAL/run_eval_hmix_dens.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=50:00:00
+#SBATCH --partition=blanca-shirts
+#SBATCH --qos=blanca-shirts
+#SBATCH --account=blanca-shirts
+#SBATCH --gres=gpu
+#SBATCH --job-name=gopal_hmix_dens
+#SBATCH --output=slurm_codes/hmix_dens.log
+
+module purge
+module avail
+ml anaconda
+conda activate evaluator-blanca
+
+python hmix_dens_calc.py
+echo "It finished"
+
+sacct --format=jobid,jobname,cputime,elapsed
